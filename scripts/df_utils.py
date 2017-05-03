39daab50e5093fc7df7bca9d0c14d7c3b7235bd4
belyguerra@berkeley.edu <bguerra@nx6.priv>
2017-05-03 12:31:20 -0700

    streamlined gaze data preprocessing steps

---
+++ scripts/df_utils.py
@@ -0,0 +1,141 @@
+import TI_settings as settings #use PM_settings for matrices
+import os
+
+
+# collects the names of each subject's raw data files
+def get_files(subjid, block):
+    txtfiles = [os.path.join(settings.rawfiles_dir, f) for f in os.listdir(settings.rawfiles_dir)
+        if os.path.isfile(os.path.join(settings.rawfiles_dir, f)) and '.txt' in f
+        and block in f and str(subjid) in f]
+
+    gazefile = None
+    blinkfile = None
+    pupilfile = None
+    behavfile = None
+
+    for txtfile in txtfiles:
+        if 'gaze' in txtfile:
+            gazefile = txtfile
+        if 'blink' in txtfile:
+            blinkfile = txtfile
+        if 'pupil' in txtfile:
+            pupilfile = txtfile
+        if 'behav' in txtfile:
+            behavfile = txtfile
+
+    print "data for block: %s" %block
+    print gazefile
+    print blinkfile
+    print pupilfile
+    print behavfile
+
+    return [gazefile, blinkfile, pupilfile, behavfile]
+
+ # Read each subject's raw data files. Headers are specified in settings
+def read_tsv(filename, headers):
+    rows = []
+
+    with open(filename, 'r') as f:
+        for line in f:
+            data = line.rstrip().split('\t')
+
+            row_data = {}
+            column_cnt = 0
+            for header in headers:
+                row_data[header] = data[column_cnt]
+                column_cnt += 1
+
+            rows.append(row_data)
+
+    return rows
+
+# collects data from the raw data files ###check
+def get_new_data_row():
+    row = {}
+    for val in settings.gazecols:
+        row[val] = settings.default_value
+    #for val in settings.blinkcols:
+        #row[val] = settings.default_value
+    #for val in settings.pupilcols:
+        #row[val] = settings.default_value
+    #for val in settings.behavcols:
+        #row[val] = settings.default_value
+
+    return row
+
+# ogama needs trials starting at 1 so we modify all the trials for all processing
+def trial_reindex(row):
+    row['Trial'] = int( row['Trial']) + 1
+
+# helps combine blink/pupil data with gaze file
+def add_ocular_data_to_dataset(combined_data_set, rows):
+    for r in rows:
+        key = r['Trial'] + ':' + r['Time']
+        if key not in combined_data_set:
+            combined_data_set[key] = get_new_data_row()
+        for col, value in r.iteritems():
+            combined_data_set[key][col] = value
+
+## functions to assign a condition to each trial. conditions specified in settings.
+# sets keys for trial number
+def get_trial_dic(filename):
+    d = {}
+    with open(filename, 'r') as d_list:
+        for line in d_list:
+            (key, val) = line.rstrip().split('\t')
+            d[int(key)] = val
+    return d
+# maps the condition to the trial key
+def map_conditions(new_column, row_data, trial_dic):
+    if row_data['Trial'] not in trial_dic:
+        raise Exception('trial not found %s' % row_data['Trial']) 
+
+    row_data[new_column] = trial_dic[row_data['Trial']]
+
+
+# ogama can only read sbj ids that start with a letter
+def ogama_subject(subjid, block, row):
+    row['Subject_ogama'] = 'tp' + str(subjid) + block
+
+# add subject and block information to the files
+def subject_block(subjid, block, row):
+    row['PID'] = subjid
+    row['Block'] = block
+
+# calculate accuracy based on behavioral info
+def acc(row):
+    #raise exception if behavioral information is missing?
+    row['ACC'] = 1 if row['CorrectAnswer'] == row['SubjectResponse'] else 0
+
+# helpful functions for calcuating summary measures
+def combine_vals(val1, val2):
+    if val1 == settings.default_value:
+        return val2
+    if val2 == settings.default_value:
+        return val1
+
+    return val1 + val2
+
+def min_vals(val1, val2):
+    if val1 == settings.default_value:
+        return val2
+    if val2 == settings.default_value:
+        return val1
+
+    return min(val1, val2)
+
+# write out function to 
+def output_rows(filepath, rows):
+    if len(rows) == 0:
+        raise Exception('Nothing to write!')
+        
+    with open(filepath, 'w') as f:
+        headers = list(rows[0].keys())
+        f.write('\t'.join(headers))
+        f.write('\n')
+        
+        for row in rows:
+            values = [str(row[header]) for header in headers]
+            line = '\t'.join(values)
+            f.write(line)
+            f.write('\n')
---
+++ scripts/fixation_utils.py
@@ -0,0 +1,311 @@
+import TI_settings as settings #use PM_settings for matrices
+import os
+import numpy as np
+from collections import Counter
+
+
+# converts gaze coordinates to ogama's system
+# for simplicity we will use the same coordinate in all processing
+def ogama_coordiates(row):
+    x = float(row['Gaze(x)'])
+    y = float(row['Gaze(y)'])
+    row['Gaze(x)_ogama'] = settings.default_value
+    row['Gaze(y)_ogama'] = settings.default_value
+    if x == -1 or y == -1:
+        return
+    row['Gaze(x)_ogama'] = x + 640
+    row['Gaze(y)_ogama'] = (y*-1) + 512
+
+# infers AOI based on the gaze coordinates. AOIs defined in settings
+def add_aoi(row):
+    x = float(row['Gaze(x)_ogama'])
+    y = float(row['Gaze(y)_ogama'])
+    condition = row['Condition']
+    row['AOI'] = 'N' #nowhere
+    if x == settings.default_value or y == settings.default_value:
+        return
+    aoi_list = settings.AOIs[condition]
+    for aoi_data in aoi_list:
+        if x >= aoi_data[0] and x <= aoi_data[1] and y >= aoi_data[2] and y <= aoi_data[3]:
+            row['AOI'] = aoi_data[4]
+            return
+
+# IDs fixations - must be run before describe_fixations function
+# gaze data grouped as one fixation if within 100ms the dispertation <= 35px
+def identify_fixations(rows):
+    fixs = []
+    counter = 0
+    x = [float(row['Gaze(x)_ogama']) for row in rows]
+    y = [float(row['Gaze(y)_ogama']) for row in rows]
+    while len(x) > 1:
+        xwindow = x[0:12]
+        ywindow = y[0:12] 
+        if (abs(float(max(xwindow)) - float(min(xwindow))) <= 35) and (abs(float(max(ywindow)) - float(min(ywindow))) <= 35):
+            i = 12
+            while (abs(float(max(xwindow)) - float(min(xwindow))) <= 35) and (abs(float(max(ywindow)) - float(min(ywindow)))) <= 35 and i < len(x):
+                xwindow.append(x[i])
+                ywindow.append(y[i])
+                i += 1
+            counter += 1
+            fixs += [counter] * (len(xwindow)-1)
+            x = x[len(xwindow)-1:]
+            y = y[len(ywindow)-1:]
+        else:
+            x = x[1:]
+            y = y[1:]
+            fixs += [-1]
+
+    #classify last point
+    if len(x) != 0:
+        xwindow.append(x[0])
+        ywindow.append(y[0])
+        if (abs(float(max(xwindow)) - float(min(xwindow))) <= 35) and (abs(float(max(ywindow)) - float(min(ywindow))) <= 35):
+            fixs += [counter]
+        else:
+            fixs += [-1]
+    if len(fixs) != len(rows):
+        raise Exception('number of fixations is not the same as number of rows')
+    for i, row in enumerate(rows):
+        row['Fixation'] = fixs[i]
+
+
+# trial -> transition_cnt_dictionary
+def get_transitions(rows):
+    trial_to_data = {}
+    trial_to_transitions = {}
+
+    for row in rows:
+        aoi = row['AOI']
+        fixation = int(row['Fixation'])
+        trial_num = int(row['Trial'])
+
+        if fixation == settings.default_value or fixation == -1:
+            continue
+                
+        if trial_num not in trial_to_data:
+            trial_to_data[trial_num] = {}
+        
+        if fixation not in trial_to_data[trial_num]:
+            trial_to_data[trial_num][fixation] = []
+            
+        trial_to_data[trial_num][fixation].append(aoi)
+        
+    # iterate data and add transitions to dictionary
+    for trial_num, data in trial_to_data.iteritems():
+        
+        if trial_num not in trial_to_transitions:
+            trial_to_transitions[trial_num] = []
+        
+        sorted_fixations = sorted(data.keys())
+        if len(sorted_fixations) > 1:
+            prev_aoi = Counter(data[sorted_fixations[0]]).most_common()[0][0]
+            for i in range(1, len(sorted_fixations)):
+                next_aoi = Counter(data[sorted_fixations[i]]).most_common()[0][0]
+                trial_to_transitions[trial_num].append((prev_aoi, next_aoi))
+                prev_aoi = next_aoi
+
+    trial_to_transition_cnts = {}
+
+    for trial, list_transitions in trial_to_transitions.iteritems():
+        trial_to_transition_cnts[trial] = {t[2]:0 for t in settings.transitions}
+        for transition in list_transitions:
+            key = transition[0] + '-' + transition[1]
+            trial_to_transition_cnts[trial][key] += 1
+
+    return trial_to_transition_cnts
+
+# calculates start and duration of fixations on each AOI
+# creates an output file that can be used for plotting heatmaps
+def describe_fixations(rows):
+
+    output_fixation_data = []
+    trial_to_fixation_data = {}
+    trial_times = {}
+
+    for row in rows:
+        aoi = row['AOI']
+        fixation = int(row['Fixation'])
+        trial_num = int(row['Trial'])
+        time = float(row['Time'])
+        subject = int(row['PID'])
+        block = row['Block']
+        condition = row['Condition']
+
+        if fixation == settings.default_value or fixation == -1:
+            continue
+
+        if trial_num not in trial_times:
+            trial_times[trial_num] = []
+        trial_times[trial_num].append(time)
+
+        if trial_num not in trial_to_fixation_data:
+            trial_to_fixation_data[trial_num] = {}
+
+        if fixation not in trial_to_fixation_data[trial_num]:
+            trial_to_fixation_data[trial_num][fixation] = {
+                'aois' : [],
+                'times' : []
+            }
+
+        trial_to_fixation_data[trial_num][fixation]['aois'].append(aoi)
+        trial_to_fixation_data[trial_num][fixation]['times'].append(time)
+
+    trial_nums_sorted = sorted(trial_to_fixation_data.keys())
+    for trial_num in trial_nums_sorted:
+        fixations_sorted = sorted(trial_to_fixation_data[trial_num].keys())
+        for fixation in fixations_sorted:
+            aoi = Counter(trial_to_fixation_data[trial_num][fixation]['aois']).most_common()[0][0]
+            max_time = max(trial_to_fixation_data[trial_num][fixation]['times'])
+            min_time = min(trial_to_fixation_data[trial_num][fixation]['times'])
+            duration = max_time - min_time
+            time_start_fixation = min_time - min(trial_times[trial_num])
+
+            data = {
+                'PID' : subject,
+                'Block' : block,
+                'Trial' : trial_num,
+                'Condition' : condition,
+                'Fixation' : fixation,
+                'AOI' : aoi,
+                'Fixation_Dur' : duration,
+                'Fixation_Start' : time_start_fixation
+            }
+            output_fixation_data.append(data)
+
+    return output_fixation_data
+
+
+
+def summary_gaze_data(rows):
+    trial_to_data = {}
+    trial_to_aois = {}
+    trial_to_default_vals = {}
+    for row in rows:
+        trial = int(row['Trial'])
+        if trial not in trial_to_data:
+            trial_to_data[trial]= {
+                'TotalFixations': 0, 
+                'TotalFixations_N': 0, 
+                'TotalFixations_R1': 0,
+                'TotalFixations_R2': 0, 
+                'TotalFixations_I1': 0,
+                'TotalFixations_I2': 0, 
+                'TotalFixations_Q': 0,
+                'TotalFixTime': 0, 
+                'TotalFixTime_N': 0, 
+                'TotalFixTime_R1': 0,
+                'TotalFixTime_R2': 0, 
+                'TotalFixTime_I1': 0,
+                'TotalFixTime_I2': 0, 
+                'TotalFixTime_Q': 0,
+                'Time_toFirst_Fix_R1': settings.default_value,
+                'Time_toFirst_Fix_R2': settings.default_value, 
+                'Time_toFirst_Fix_I1': settings.default_value,
+                'Time_toFirst_Fix_I2': settings.default_value, 
+                'Time_toFirst_Fix_Q': settings.default_value,
+                'Time_toFirst_Fix_N': settings.default_value,
+                'VisualSearch_R' : settings.default_value,
+                'SearchTime_R': settings.default_value,
+                'VisualSearch_R' : settings.default_value,
+                'SearchTime_R': settings.default_value
+            }
+            trial_to_default_vals[trial] = {}
+
+
+        if trial not in trial_to_aois:
+            trial_to_aois[trial] = []
+
+        trial_to_data[trial]['TotalFixations'] += 1
+        aoi = row['AOI']
+        key = 'TotalFixations_' + aoi
+        trial_to_data[trial][key] += 1
+        start_time = row['Fixation_Start']
+
+        trial_to_aois[trial].append((aoi, start_time))
+
+        duration = row['Fixation_Dur']
+        trial_to_data[trial]['TotalFixTime'] += duration
+        key = 'TotalFixTime_' + aoi
+        trial_to_data[trial][key] += duration
+        
+        key = 'Time_toFirst_Fix_'+ aoi
+        if key not in trial_to_default_vals[trial]:
+            trial_to_default_vals[trial][key] = True
+            trial_to_data[trial][key] = start_time
+
+    for trial, aois_times in trial_to_aois.iteritems():
+        trial_to_data[trial]['VisualSearch_R'],trial_to_data[trial]['SearchTime_R']  = get_visual_search(aois_times)
+        trial_to_data[trial]['VisualSearch_I'],trial_to_data[trial]['SearchTime_I']  = get_last_I(aois_times)
+
+    return trial_to_data
+
+def get_visual_search(aois_times):
+    visual_search = settings.default_value
+    search_time = settings.default_value
+
+    start = -1
+    index = -1
+    match_count = 0
+    for aoi, time in aois_times:
+        index += 1
+
+        if aoi == 'N' or aoi == 'Q':
+            continue
+        elif aoi[0] != 'R':
+            match_count = 0
+            start = -1
+        else:
+            match_count += 1
+            if start == -1:
+                start = index
+                #time_start = time
+
+            if match_count == 3:
+                visual_search = start
+                search_time = time
+                break
+
+    return visual_search, search_time
+
+def get_last_I(aois_times):
+    search_last_I = settings.default_value
+    time_last_I = settings.default_value
+
+    index = -1
+    
+    for aoi, time in aois_times:
+        index += 1
+
+        if aoi[0] == 'I':
+            search_last_I = index
+            time_last_I = time
+        
+    return search_last_I, time_last_I
+
+def get_last_Itrans(aois_times):
+    search_last_Itrans = settings.default_value
+    time_last_Itrans = settings.default_value
+
+    start = -1
+    index = -1
+    match_count = 0
+    for aoi, time in aois_times:
+        index += 1
+
+        if aoi == 'N' or aoi == 'Q':
+            continue
+        elif aoi[0] != 'I':
+            match_count = 0
+            start = -1
+        else:
+            match_count += 1
+            if start == -1:
+                start = index
+                #time_start = time
+
+            if match_count == 2:
+                visual_search = start
+                search_time = time
+                break
+
+    return visual_search, search_time
